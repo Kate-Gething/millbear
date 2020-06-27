768193c1986cc0e7c73e6c83857d846e51d35201
Kate-Gething <61317358+Kate-Gething@users.noreply.github.com>
2020-06-27 20:16:08 +0100

    Update cookies.js

--- cookies.js
+++ cookies.js
@@ -1,11 +1,1 @@
-// <script type="text/javascript" id="cookiebanner"
-//     src="https://cdn.jsdelivr.net/gh/dobarkod/cookie-banner@1.2.2/dist/cookiebanner.min.js"
-//     data-height="20px" data-position="top"
-//     data-message="We use cookies to improve your browsing experience.">
-// </script>
 
-
-<script type="text/javascript">
-      var options = { message: "We use cookies to enhance your experience.", moreinfo: "/about/cookies" };
-      var cb = new Cookiebanner(options); cb.run();
-</script>
