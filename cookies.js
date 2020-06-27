4d36b5becb9ed62a78d86177d25d15ee264355a8
Kate-Gething <61317358+Kate-Gething@users.noreply.github.com>
2020-06-27 20:31:26 +0100

    Update cookies.js

--- cookies.js
+++ cookies.js
@@ -1,1 +1,4 @@
-
+<script type="text/javascript">
+      var options = { message: "We use cookies to enhance your experience.", moreinfo: "/about/cookies" };
+      var cb = new Cookiebanner(options); cb.run();
+</script>
