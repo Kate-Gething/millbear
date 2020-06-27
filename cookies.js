801b98855083b2101cda44e7eecbe57862bd220d
Kate-Gething <61317358+Kate-Gething@users.noreply.github.com>
2020-06-27 19:47:10 +0100

    Update cookies.js

--- cookies.js
+++ cookies.js
@@ -1,7 +1,5 @@
-if (localStorage.getItem(‘cookieSeen’) != ‘shown’) {
-  $(‘.cookie-banner’).delay(2000).fadeIn();
-  localStorage.setItem(‘cookieSeen’,’shown’)
-};
-$(‘.close’).click(function() {
-  $(‘.cookie-banner’).fadeOut();
-})
+<script type="text/javascript" id="cookiebanner"
+    src="https://cdn.jsdelivr.net/gh/dobarkod/cookie-banner@1.2.2/dist/cookiebanner.min.js"
+    data-height="20px" data-position="top"
+    data-message="We use cookies to improve your browsing experience.">
+</script>
